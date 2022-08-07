6af5331f52019a31fc3dc0ac64fb3f95463f79fc
patthamawa <patthama.wa.65@ubu.ac.th>
2022-08-07 19:53:10 +0700

    add

--- src/MyFooter.js
+++ src/MyFooter.js
@@ -3,7 +3,7 @@ import './App.css';
 function MyFooter() {
     return (
       <div className='My-footer'>
-        <span>Developed by Anusorn B. @ 2022</span>
+        <span>Developed by Patthama @ 2022</span>
       </div>
     );
 }
